6a491c170491538d3ccdd60ab5b36fc6159b015b
PhuongLe <phuongle@macOS.local>
2020-08-29 13:34:16 +0700

    [Task] Init Application

---
+++ src/test/java/com/ple/example/icommerce/ICommerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ple.example.icommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ICommerceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
